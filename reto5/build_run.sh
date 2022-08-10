7452b0c606a10631a1243182c370a13ec59b9c10
martiningaramo <tinchogaramo@gmail.com>
2022-08-10 16:20:50 -0300

    Challenge 3: Reto 5 v1.1

--- reto5/build_run.sh
+++ reto5/build_run.sh
@@ -2,14 +2,14 @@
 
 read -p "Introduce tu usuario de DockerHub: " nombre
 
-docker build -t nodejs_bootcamp .
+docker build -t nodejs_bootcamp_mingaramo .
 
-docker run -d -p 4000:4000 --name nodejs_cont nodejs_bootcamp
+docker run -d -p 4000:4000 --name nodejs_cont nodejs_bootcamp_mingaramo
 
 docker ps
 
 docker logs nodejs_cont
 
-docker tag nodejs_bootcamp $nombre/nodejs_bootcamp:v1.0.0
+docker tag nodejs_bootcamp_mingaramo $nombre/nodejs_bootcamp_mingaramo:v1.0.0
 
-docker push $nombre/nodejs_bootcamp:v1.0.0
+docker push $nombre/nodejs_bootcamp_mingaramo:v1.0.0
--- reto5/solucion.md
+++ reto5/solucion.md
@@ -2,14 +2,11 @@
 
 ### Reto 5
 
-#### Build & Run de la imagen nodejs
+#### Prueba Build/Run/Registry a traves de script build_run.sh
 ![Esta es una imagen](01.png)
 
 #### Abriendo la aplicación en un browser
 ![Esta es una imagen](02.png)
 
-#### Prueba Build/Run/Registry a traves de script build_run.sh
-![Esta es una imagen](03.png)
-
 #### Imagen subida a DockerHub
-![Esta es una imagen](04.png)
+![Esta es una imagen](03.png)
